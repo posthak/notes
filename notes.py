1abd646ea28b142953fc5533476b75eec6331985
posthak <adler_m@mail.ru>
2023-06-08 19:50:59 +0200

    Note added

--- notes.py
+++ notes.py
@@ -1,65 +1,143 @@
 
+from datetime import date
+import json
 import os
+from time import strptime
+import random
+import string
 
 
 def clear(): return os.system('clear')
 
 
+def random_number():
+    return ''.join(random.choices(string.ascii_letters, k=5))
+
+
 def AddNote(fileName, title, body):
-    date = '2023.06.06'
-    id = '1'
-    with open(fileName, "a", encoding="utf8") as file:
-        file.write(id + ";" + date + ";" + title + ";" + body + ";" + "\n")
-    print('Contact was added!')
+    users = json.load(open(fileName))
+    users.append({
+        "id": random_number(),
+        "title": title,
+        "body": body,
+        "date": str(date.today())
+    })
+
+    with open(fileName, 'w') as outfile:
+        json.dump(users, outfile, indent=4,
+                  ensure_ascii=False)
+    print('Заметка успешно сохранена')
 
 
-def ShowAll(name):
-    with open(name, "r", encoding="utf8") as file:
-        list(map(lambda x: print(x, end=""), file.read()))
+def FindNoteByDate(fileName, date):
+    status = False
+    list = json.load(open(fileName))
+    listNew = []
+    for d in list:
+        if d["date"] == date:
+            listNew.append({
+                "id": d["id"],
+                "title": d["title"],
+                "body": d["body"],
+                "date": d["date"]
+            })
+            status = True
+    for i in sorted(listNew, key=lambda i: strptime(i["date"], "%Y-%m-%d")):
+        print(i)
+    if not status:
+        print('Заметка не найдена')
 
 
-fName = 'note.csv'
+def DelNote(fileName, id):
+    status = False
+    list = json.load(open(fileName))
+    listNew = []
+    for d in list:
+        if d["id"] != id:
+            listNew.append({
+                "id": d["id"],
+                "title": d["title"],
+                "body": d["body"],
+                "date": d["date"]
+            })
+        elif d["id"] == id:
+            status = True
+    with open(fileName, 'w') as outfile:
+        json.dump(listNew, outfile, indent=4,
+                  ensure_ascii=False)
+    if status:
+        print('Заметка удалена')
+    else:
+        print('Заметка не найдена')
+
+
+def CheckNote(fileName, id):
+    status = False
+    list = json.load(open(fileName))
+    for d in list:
+        if d["id"] == id:
+            status = True
+    return status
+
+
+def EditNote(fileName, id, body):
+    list = json.load(open(fileName))
+    for d in list:
+        if d["id"] == id:
+            d["body"] = body
+            d["date"] = str(date.today())
+    with open(fileName, 'w') as outfile:
+        json.dump(list, outfile, indent=4,
+                  ensure_ascii=False)
+    print('Заметка изменена')
+
+
+def DisplayAllNote(name):
+    with open(name, "r") as file:
+        list = json.load(file)
+        for i in sorted(list, key=lambda i: strptime(i["date"], "%Y-%m-%d")):
+            print(i)
+
+
+fName = 'file.json'
 clear()
+
+if not os.path.exists(fName):
+    with open(fName, "w") as f:
+        f.write("[]")
+
 while True:
     menuitems = \
         ('1 - Создать заметку',
-            '2 - Сохранить заметку',
-            '3 - Просмотреть список заметок',
-            '4 - Просмотреть заметку по дате',
-            '5 - Редактировать заметку',
-            '6 - Удалить заметку',
-            '7 - Выход')
+            '2 - Просмотреть список заметок',
+            '3 - Просмотреть заметку по дате',
+            '4 - Редактировать заметку',
+            '5 - Удалить заметку',
+            '6 - Выход')
 
+    print("\n")
     print('Введите номер действия:')
     list(map(lambda x: print(x), menuitems))
     menu = int(input())
     clear()
     if menu == 1:
-        # ReadFile(fName)
-        print('Создание заметки...')
-        title = input("Input tile: ")
-        body = input("Input body: ")
+        title = input("Введите заголовок заметки: ")
+        body = input("Введите тело заметки: ")
         AddNote(fName, title, body)
     elif menu == 2:
-        text = input("Input name: ")
-        print('Сохранение заметки...')
-        #FindByName(fName, text)
+        DisplayAllNote(fName)
     elif menu == 3:
-        text = input("Input number: ")
-        print('Вывести все заметки на экран...')
-        #FindByName(fName, text)
-        ShowAll(fName)
+        text = input("Введите дату yyyy-mm-dd: ")
+        FindNoteByDate(fName, text)
     elif menu == 4:
-        text = input("Input number: ")
-        print('Вывести заметку на кран...')
-        #FindByName(fName, text)
+        id = input("Введите id заметки: ")
+        if CheckNote(fName, id):
+            body = input("Введите тело новой заметки: ")
+            EditNote(fName, id, body)
+        else:
+            print('Заметка не найдена')
     elif menu == 5:
-        text = str(input("Input new contact:"))
-        print('Редактирование заметки...')
-        #AddLineFile(fName, text)
+        id = input("Введите id заметки: ")
+        DelNote(fName, id)
     elif menu == 6:
-        text = str(input("Input name or mob. num: "))
-        print('Удаление заметки...')
-        #DeleteContact(fName, text)
-    elif menu == 7:
         quit()
